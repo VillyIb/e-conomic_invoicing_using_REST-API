e36cff40c2506cc329ac881e48fc91c008f177ed
VillyIb <villy.ib.jorgensen@gamil.com>
2024-04-10 08:27:15 +0200

    Adds project Eu.Iamia.Utils with StringExtensions.

---
+++ src/Eu.Iamia.Utils/StringExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+
+namespace Eu.Iamia.Utils;
+
+public static class StringExtensions
+{
+    /// <summary>
+    /// Returns the provided json with newlines and indenting.
+    /// - or the provided NON-json as it is.
+    /// </summary>
+    /// <param name="json"></param>
+    /// <returns></returns>
+    public static string JsonPrettify(this string json)
+    {
+        try
+        {
+            using var jDoc = JsonDocument.Parse(json);
+            return JsonSerializer.Serialize(jDoc, new JsonSerializerOptions { WriteIndented = true });
+        }
+        catch
+        {
+            return json;
+        }
+    }
+}
