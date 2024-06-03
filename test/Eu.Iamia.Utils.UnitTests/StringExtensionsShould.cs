1107fc866fe502fdd24e3fd897d03c7c59b7f6b9
Villy_Joergensen <villy.joergensen@wsa.com>
2024-06-03 13:39:57 +0200

    Simplifies UnitTest.

--- test/Eu.Iamia.Utils.UnitTests/StringExtensionsShould.cs
+++ test/Eu.Iamia.Utils.UnitTests/StringExtensionsShould.cs
@@ -1,76 +1,43 @@
 namespace Eu.Iamia.Utils.UnitTests;
 
-public class CharComparerExact : IEqualityComparer<char>
-{
-    public bool Equals(char x, char y)
-    {
-        return x.GetHashCode() == y.GetHashCode();
-    }
-
-    public int GetHashCode(char obj)
-    {
-        return obj.GetHashCode();
-    }
-}
-
 public class StringExtensionsShould
 {
-    private static bool RightLengthOfResult(int length, string actual)
-    {
-        return length == actual.Length;
-    }
-
-    private static bool RightLengthFromOriginalContent(int affix, string? input, int length, string actual)
-    {
-        var charComparer = new CharComparerExact();
-        return (length - affix) == actual.Intersect(input ?? string.Empty, charComparer ).Count();
-    }
-
-    private static bool RightNumberOfAffixCharacters(int expectedPostfixCount, string actual, char affixChar)
-    {
-        return expectedPostfixCount == actual.Count(ch => ch == affixChar);
-    }
-
     [Theory]
-    [InlineData(0, "1234", 0)]
-    [InlineData(0, "1234", 1)]
-    [InlineData(0, "1234", 2)]
-    [InlineData(0, "1234", 3)]
-    [InlineData(0, "1234", 4)]
-    [InlineData(1, "1234", 5)]
-    [InlineData(4, "", 4)]
-    [InlineData(4, null, 4)]
-    [InlineData(0, "aAÊ∆¯ÿÂ≈" , 8)]
-    public void TrimToLength_Returns_ExpectedLength_With_VoidPostfixFilled(int expectedPostfixCount, string? input, int length)
+    [InlineData("", 0, "AbCdEf")]
+    [InlineData("A", 1, "AbCdEf")]
+    [InlineData("Ab", 2, "AbCdEf")]
+    [InlineData("AbC", 3, "AbCdEf")]
+    [InlineData("AbCd", 4, "AbCdEf")]
+    [InlineData("AbCdE", 5, "AbCdEf")]
+    [InlineData("AbCdEf", 6, "AbCdEf")]
+    [InlineData("AbCdEf@", 7, "AbCdEf")]
+    [InlineData("AbCdEf@@", 8, "AbCdEf")]
+    [InlineData("@@@@", 4, "")]
+    [InlineData("@@@@", 4, null)]
+    public void TrimToLength_Returns_RightValue(string expected, int length, string? input)
     {
         const char postfix = '@';
-
         var actual = input.TrimToLength(length, postfix);
-
-        Assert.True(RightLengthOfResult(length, actual));
-        Assert.True(RightLengthFromOriginalContent(expectedPostfixCount, input, length, actual));
-        Assert.True(RightNumberOfAffixCharacters(expectedPostfixCount, actual, postfix));
+        Assert.Equal(expected, actual);
     }
 
     [Theory]
-    [InlineData(0, "1234", 0)]
-    [InlineData(0, "1234", 1)]
-    [InlineData(0, "1234", 2)]
-    [InlineData(0, "1234", 3)]
-    [InlineData(0, "1234", 4)]
-    [InlineData(1, "1234", 5)]
-    [InlineData(4, "", 4)]
-    [InlineData(4, null, 4)]
-    [InlineData(0, "aAÊ∆¯ÿÂ≈", 8)]
-    public void TrimNumberToLength_Returns_ExpectedLength_With_VoidPrefixFilled(int expectedPrefixCount, string? input, int length)
+    [InlineData("", 0, "AbCdEf")]
+    [InlineData("A", 1, "AbCdEf")]
+    [InlineData("Ab", 2, "AbCdEf")]
+    [InlineData("AbC", 3, "AbCdEf")]
+    [InlineData("AbCd", 4, "AbCdEf")]
+    [InlineData("AbCdE", 5, "AbCdEf")]
+    [InlineData("AbCdEf", 6, "AbCdEf")]
+    [InlineData("@AbCdEf", 7, "AbCdEf")]
+    [InlineData("@@AbCdEf", 8, "AbCdEf")]
+    [InlineData("@@@@", 4, "")]
+    [InlineData("@@@@", 4, null)]
+    public void TrimToNumberLength_Returns_RightValue(string expected, int length, string? input)
     {
-        const char prefix = '@';
-
-        var actual = input.TrimNumberToLength(length, prefix);
-
-        Assert.True(RightLengthOfResult(length, actual));
-        Assert.True(RightLengthFromOriginalContent(expectedPrefixCount, input, length, actual));
-        Assert.True(RightNumberOfAffixCharacters(expectedPrefixCount, actual, prefix));
+        const char postfix = '@';
+        var actual = input.TrimNumberToLength(length, postfix);
+        Assert.Equal(expected, actual);
     }
 
     [Fact]
