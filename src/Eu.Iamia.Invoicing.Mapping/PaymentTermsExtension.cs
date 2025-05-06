0070a8e6bc4d958713e4759c89ebee903f748fc7
Villy Ib Jørgensen <villy4.ib.jorgensen@gmail.com>
2025-05-06 14:35:14 +0200

    Adds extension.

---
+++ src/Eu.Iamia.Invoicing.Mapping/PaymentTermsExtension.cs
@@ -0,0 +1,19 @@
+﻿using Eu.Iamia.Invoicing.E_Conomic.Gateway.V2.Contract.DTO.Invoices.drafts.post;
+
+namespace Eu.Iamia.Invoicing.Mapping;
+
+public static class PaymentTermsExtension
+{
+    public static PaymentTerms ToPaymentTerms(this Application.Contract.DTO.PaymentTermDto paymentTerm)
+    {
+        return new PaymentTerms()
+        {
+            DaysOfCredit = paymentTerm.DaysOfCredit,
+            Name = paymentTerm.Name,
+            PaymentTermsNumber = int.Parse(paymentTerm.PaymentTermNumber),
+            PaymentTermsType =
+                Enum.Parse<PaymentTermsType>(paymentTerm
+                    .PaymentTermsType)
+        };
+    }
+}
