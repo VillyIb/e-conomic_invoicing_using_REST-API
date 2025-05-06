d2aae8acedf8952223c0305c72070e8923eb2ca3
Villy Ib Jørgensen <villy4.ib.jorgensen@gmail.com>
2025-05-06 11:37:23 +0200

    Code cleanup.

--- src/Eu.Iamia.Invoicing.E-Conomic.Gateway.V2.Contract/IEconomicGatewayV2.cs
+++ src/Eu.Iamia.Invoicing.E-Conomic.Gateway.V2.Contract/IEconomicGatewayV2.cs
@@ -9,19 +9,46 @@ namespace Eu.Iamia.Invoicing.E_Conomic.Gateway.V2.Contract;
 
 public interface IEconomicGatewayV2
 {
-    Task<CustomersHandle> ReadCustomers(int page, int pageSize, CancellationToken cancellationToken = default);
+    // TODO Consider call it GetCustomers(...)
+    Task<CustomersHandle> ReadCustomers(
+        int page, 
+        int pageSize, 
+        CancellationToken cancellationToken = default
+    );
 
-    Task<ProductsHandle> ReadProducts(int page, int pageSize, CancellationToken cancellationToken = default);
+    Task<ProductsHandle> ReadProducts(
+        int page, 
+        int pageSize, 
+        CancellationToken cancellationToken = default
+    );
 
-    Task<PaymentTermsHandle?> ReadPaymentTerms(int page, int pageSize, CancellationToken cancellationToken = default);
+    Task<PaymentTermsHandle?> ReadPaymentTerms(
+        int page, 
+        int pageSize, 
+        CancellationToken cancellationToken = default
+    );
 
-    Task<IDraftInvoice?> PostDraftInvoice(Invoice restApiInvoice, int sourceFileNumber, CancellationToken cancellationToken);
+    Task<IDraftInvoice?> PostDraftInvoice(
+        Invoice restApiInvoice, 
+        int sourceFileNumber, 
+        CancellationToken cancellationToken
+    );
 
-    Task<Contract.DTO.Invoices.booked.get.BookedInvoicesHandle> ReadBookedInvoices(int page, int pageSize, IInterval<DateTime> dateRange, CancellationToken cancellationToken = default);
+    Task<Contract.DTO.Invoices.booked.get.BookedInvoicesHandle> ReadBookedInvoices(
+        int page, 
+        int pageSize, 
+        IInterval<DateTime> dateRange, 
+        CancellationToken cancellationToken = default
+    );
 
-    Task<Contract.DTO.Invoices.booked.bookedInvoiceNumber.get.BookedInvoice> ReadBookedInvoice(int invoiceNumber, CancellationToken cancellationToken = default);
+    Task<Contract.DTO.Invoices.booked.bookedInvoiceNumber.get.BookedInvoice> ReadBookedInvoice(
+        int invoiceNumber, 
+        CancellationToken cancellationToken = default
+    );
 
     Task<int> LoadPaymentTermsCache();
 
-    PaymentTerm? GetPaymentTerm(int paymentTermsNumber);
+    PaymentTerm? GetPaymentTerm(
+        int paymentTermsNumber
+    );
 }
