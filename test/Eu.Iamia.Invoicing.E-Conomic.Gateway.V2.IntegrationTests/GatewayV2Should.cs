51eb03e4b3191d316974ac8ccf49ece79cf436bf
Villy Ib Jørgensen <villy4.ib.jorgensen@gmail.com>
2025-07-05 09:39:53 +0200

    Adds Integration Tests.

--- test/Eu.Iamia.Invoicing.E-Conomic.Gateway.V2.IntegrationTests/GatewayV2Should.cs
+++ test/Eu.Iamia.Invoicing.E-Conomic.Gateway.V2.IntegrationTests/GatewayV2Should.cs
@@ -1,6 +1,6 @@
 ﻿using Eu.Iamia.Invoicing.E_Conomic.Gateway.V2.Contract;
-using Refit;
 using Eu.Iamia.Utils;
+using Refit;
 using System.Net;
 
 namespace Eu.Iamia.Invoicing.E_Conomic.Gateway.V2.IntegrationTests;
@@ -19,25 +19,65 @@ public class GatewayV2Should
         _sut = setup.GetService<IEconomicGatewayV2>();
     }
 
-    // this should be a unit test validating the generated query.
-    //[Theory]
-    //[InlineData(1, 66, "2024-01-01", "2024-12-31")]
-    //public async Task ReadBookedInvoices(int page, int pageSize, string from, string to)
-    //{
-    //    var dateRange = Interval<DateTime>.Create(DateTime.Parse(from), DateTime.Parse(to));
-    //    var bookedInvoicesHandle = await ((GatewayV2TestVariant)_sut).ReadBookedInvoices(page, pageSize, dateRange, _cts.Token);
+    [Theory]
+    [InlineData(0, 20, true)]
+    [InlineData(99, 20, false)]
+    public async Task GetPaymentTerms(int page, int pageSize, bool expectData)
+    {
+        var stream = await _sut.GetPaymentTerms(page, pageSize);
+        Assert.NotNull(stream);
 
-    //    Assert.NotNull(bookedInvoicesHandle);
-    //    Assert.True(bookedInvoicesHandle.Invoices.Any());
-    //    Assert.Equal(pageSize, bookedInvoicesHandle.Invoices.Length);
-    //}
+        if (expectData)
+        {
+            Assert.True(stream.PaymentTerms.Any());
+        }
+        else
+        {
+            Assert.False(stream.PaymentTerms.Any());
+        }
+    }
+
+    [Theory]
+    [InlineData(000, 20, true)] // test environment only contains 5 customers no. 1..5
+    [InlineData(999, 20, false)]
+    public async Task GetCustomers(int page, int pageSize, bool expectCustomers)
+    {
+        var customersHandle = await _sut.GetCustomers(page, pageSize);
+
+        Assert.NotNull(customersHandle);
+        Assert.Equal(expectCustomers, customersHandle.Customers.Any());
+    }
+
+    [Theory]
+    [InlineData(00, 20, true)] // test environment contains product 1..7
+    [InlineData(99, 20, false)]
+    public async Task GetProducts(int page, int pageSize, bool expectProducts)
+    {
+        var producsHandle = await _sut.GetProducts(page, pageSize);
+
+        Assert.NotNull(producsHandle);
+        Assert.Equal(expectProducts, producsHandle.Products.Any());
+    }
+
+    [Theory]
+    [InlineData(00, 20, "2024-01-01", "2024-12-31", true)]
+    [InlineData(99, 20, "2024-01-01", "2024-12-31", false)]
+    public async Task GetBookedInvoices(int page, int pageSize, string _dateFrom, string _DateTo, bool expectInvoices)
+    {
+        var dateFrom = DateTime.Parse(_dateFrom);
+        var dateTo = DateTime.Parse(_DateTo);
+        var interval = Interval<DateTime>.Create(dateFrom, dateTo);
+        var bookedInvoicesHandle = await _sut.GetBookedInvoices(page, pageSize, interval);
+        Assert.NotNull(bookedInvoicesHandle);
+        Assert.Equal(expectInvoices, bookedInvoicesHandle.Invoices.Any());
+    }
 
     [Theory]
     [InlineData(2024072, true)]
     [InlineData(9999999, false)]
     public async Task GetBookedInvoice(int invoiceNumber, bool expetFound)
     {
-        if(expetFound)
+        if (expetFound)
         {
             var bookedInvoice = await _sut.GetBookedInvoice(invoiceNumber);
             Assert.NotNull(bookedInvoice);
@@ -48,25 +88,6 @@ public class GatewayV2Should
         {
             ApiException ex = await Assert.ThrowsAsync<ApiException>(async () => await _sut.GetBookedInvoice(invoiceNumber));
             Assert.Equal(HttpStatusCode.NotFound, ex.StatusCode);
-        }
-    }
-
-    [Theory]
-    [InlineData(0, 20, true)] // test environment only contains 5 customers no. 1..5
-    [InlineData(99, 20, false)]
-    public async Task GetCustomers(int page, int pageSize, bool expectCustomers)
-    {
-        
-        var customersHandle = await _sut.GetCustomers(page, pageSize);
-
-        Assert.NotNull(customersHandle);
-        if (expectCustomers)
-        {
-            Assert.True(customersHandle.Customers.Any());
-        }
-        else
-        {
-            Assert.False(customersHandle.Customers.Any());
         }
     }
 
