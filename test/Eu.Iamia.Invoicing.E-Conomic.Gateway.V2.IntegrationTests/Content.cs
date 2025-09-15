c2849ef2c41c613465f9e7ca017b7e6eeb70a55f
Villy Ib Jørgensen <villy4.ib.jorgensen@gmail.com>
2025-09-15 16:53:06 +0200

    Attempt to deserialize result from errors, doesn't work because the excepthion thrown is from deserialization.

---
+++ test/Eu.Iamia.Invoicing.E-Conomic.Gateway.V2.IntegrationTests/Content.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace Eu.Iamia.Invoicing.E_Conomic.Gateway.V2.IntegrationTests;
+
+    public class Content
+    {
+        public string message { get; set; }
+        public string errorCode { get; set; }
+        public string developerHint { get; set; }
+        public string logId { get; set; }
+        public int httpStatusCode { get; set; }
+        public string[] errors { get; set; }
+        public DateTime logTime { get; set; }
+        public string schemaPath { get; set; }
+
+    public static Content? FromJson(string json)
+    {
+        var options = new JsonSerializerOptions
+        {
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault,
+            Converters = { new JsonStringEnumConverter() },
+        };
+
+        var content = JsonSerializer.Deserialize<Content>(json, options);
+        return content;
+    }
+
+    public override string ToString()
+        => JsonSerializer.Serialize(this, new JsonSerializerOptions
+        {
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault,
+            WriteIndented = true
+        });
+}
+
+
