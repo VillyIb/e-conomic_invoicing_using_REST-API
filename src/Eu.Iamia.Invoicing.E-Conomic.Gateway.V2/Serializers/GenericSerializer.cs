ef204a3edef9a9595ff3a6a61d90936bdff5eb3d
Villy Ib Jørgensen <villy4.ib.jorgensen@gmail.com>
2025-09-16 22:34:12 +0200

    Replaced specific serializers with generic.

---
+++ src/Eu.Iamia.Invoicing.E-Conomic.Gateway.V2/Serializers/GenericSerializer.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+
+namespace Eu.Iamia.Invoicing.E_Conomic.Gateway.V2.Serializers;
+
+public static class GenericSerializer<T> where T : class
+{
+    public static async Task<T?> DeserializeAsync(Stream utf8Json, CancellationToken cancellationToken)
+    {
+#if DEBUG && false
+        // Option to inspect payload of utf8Json stream.
+        var reader = new StreamReader(utf8Json);
+        var json = await reader.ReadToEndAsync(cancellationToken);
+
+        utf8Json.Position = 0;
+#endif
+        var result = await JsonSerializer.DeserializeAsync<T>(utf8Json, JsonSerializerOptionsCache.Options, cancellationToken) ;
+
+        return result;
+    }
+
+}
---
+++ src/Eu.Iamia.Invoicing.E-Conomic.Gateway.V2/Serializers/JsonSerializerOptionsCache.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json;
+
+namespace Eu.Iamia.Invoicing.E_Conomic.Gateway.V2.Serializers;
+
+internal static class JsonSerializerOptionsCache
+{
+    private static JsonSerializerOptions? cachedOptions = null;
+
+    internal static JsonSerializerOptions Options => cachedOptions ??= new JsonSerializerOptions();
+}
