e29d6d0122b409e06625621826ce5e32f24a071c
Villy Ib Jørgensen <villy4.ib.jorgensen@gmail.com>
2025-09-16 22:34:54 +0200

    Adds new contract.

---
+++ src/Eu.Iamia.Invoicing.E-Conomic.Gateway.V2.Contract/DTO/Invoices.drafts.draftInvoiceNumber.get/DraftInvoice.cs
@@ -0,0 +1,76 @@
+﻿namespace Eu.Iamia.Invoicing.E_Conomic.Gateway.V2.Contract.DTO.Invoices.drafts.draftInvoiceNumber.get;
+
+// see: https://restapi.e-conomic.com/schema/invoices.drafts.draftInvoiceNumber.get.schema.json
+
+    public class DraftInvoice
+    {
+        public int          draftInvoiceNumber       { get; set; }
+        public int          orderNumber               { get; set; }
+        public string       date                      { get; set; }
+        public string       currency                  { get; set; }
+        public float        exchangeRate              { get; set; }
+        public float        netAmount                 { get; set; }
+        public float        netAmountInBaseCurrency   { get; set; }
+        public float        grossAmount               { get; set; }
+        public float        grossAmountInBaseCurrency { get; set; }
+        public float        vatAmount                 { get; set; }
+        public float        roundingAmount            { get; set; }
+        public float        remainder                 { get; set; }
+        public float        remainderInBaseCurrency   { get; set; }
+        public string       dueDate                   { get; set; }
+        public Paymentterms paymentTerms              { get; set; }
+        public Customer     customer                  { get; set; }
+        public Recipient    recipient                 { get; set; }
+        public Notes        notes                     { get; set; }
+        public Layout       layout                    { get; set; }
+        public Pdf          pdf                       { get; set; }
+        public string       sent                      { get; set; }
+        public string       self                      { get; set; }
+
+        public static readonly DraftInvoice NullDraftInvoice = new();
+    }
+
+public class Paymentterms
+{
+    public int paymentTermsNumber { get; set; }
+    public int daysOfCredit { get; set; }
+    public string name { get; set; }
+}
+
+public class Customer
+{
+    public int customerNumber { get; set; }
+    public string name { get; set; }
+    public string self { get; set; }
+}
+
+public class Recipient
+{
+    public string name { get; set; }
+    public string address { get; set; }
+    public string postalCode { get; set; }
+    public string city { get; set; }
+    public string country { get; set; }
+}
+
+public class Notes
+{
+    public string text { get; set; }
+}
+
+public class Layout
+{
+    public int layoutNumber { get; set; }
+    public string name { get; set; }
+}
+
+public class Pdf
+{
+    public string self { get; set; }
+    public string download { get; set; }
+}
+
+public class References
+{
+    public string other { get; set; }
+}
---
+++ src/Eu.Iamia.Invoicing.E-Conomic.Gateway.V2.Contract/DTO/Invoices.drafts.get/DraftInvoicesHandle.cs
@@ -0,0 +1,112 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Eu.Iamia.Invoicing.E_Conomic.Gateway.V2.Contract.DTO.Invoices.drafts.get;
+
+
+public class DraftInvoicesHandle
+{
+    [JsonPropertyName("collection")] public DraftInvoice[] Invoices { get; set; } = [];
+    public                                  Pagination      pagination { get; set; }
+    public                                  string          self { get; set; }
+
+    public static readonly DraftInvoicesHandle NullBookedInvoicesHandle = new();
+}
+
+public class Pagination
+{
+    public int skipPages { get; set; }
+    public int pageSize { get; set; }
+    public int maxPageSizeAllowed { get; set; }
+    public int results { get; set; }
+    public int resultsWithoutFilter { get; set; }
+    public string firstPage { get; set; }
+    public string previousPage { get; set; }
+    public string nextPage { get; set; }
+    public string lastPage { get; set; }
+}
+
+public class DraftInvoice
+{
+    public int draftInvoiceNumber { get; set; }
+    public int orderNumber { get; set; }
+    public string date { get; set; }
+    public string currency { get; set; }
+    public float exchangeRate { get; set; }
+    public float netAmount { get; set; }
+    public float netAmountInBaseCurrency { get; set; }
+    public float grossAmount { get; set; }
+    public float grossAmountInBaseCurrency { get; set; }
+    public float vatAmount { get; set; }
+    public float roundingAmount { get; set; }
+    public float remainder { get; set; }
+    public float remainderInBaseCurrency { get; set; }
+    public string dueDate { get; set; }
+    public Paymentterms paymentTerms { get; set; }
+    public Customer customer { get; set; }
+    public Recipient recipient { get; set; }
+    public Notes notes { get; set; }
+    public References references { get; set; }
+    public Layout layout { get; set; }
+    public Pdf pdf { get; set; }
+    public string sent { get; set; }
+    public string self { get; set; }
+}
+
+public class Paymentterms
+{
+    public int paymentTermsNumber { get; set; }
+    public int daysOfCredit { get; set; }
+    public string name { get; set; }
+}
+
+
+public class Customer
+{
+    public int customerNumber { get; set; }
+    public string name { get; set; }
+    public string self { get; set; }
+}
+
+public class Recipient
+{
+    public string address { get; set; }
+    public string city { get; set; }
+    public string zip { get; set; }
+    public string name { get; set; }
+    public VatZone vatZone { get; set; }
+}
+
+
+public class VatZone
+{
+    public bool enabledForCustomer { get; set; }
+    public bool enabledForSupplier { get; set; }
+    public string name { get; set; }
+    public int vatZoneNumber { get; set; }
+}
+
+public class Notes
+{
+    public string heading { get; set; }
+    public string textLine1 { get; set; }
+}
+
+public class References
+{
+    public string other { get; set; }
+}
+
+public class Layout
+{
+    public int layoutNumber { get; set; }
+}
+
+public class Pdf
+{
+    public string self { get; set; }
+    public string url { get; set; }
+    public string fileName { get; set; }
+    public string contentType { get; set; }
+}
+
+
